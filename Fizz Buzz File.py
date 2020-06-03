727678f3c4a5f27f8e77b3c029d32326cb8e8d5e
Thomas <jackson996@btinternet.com>
2020-06-03 11:15:13 +0100

    changed multiples into variables

--- Fizz Buzz File.py
+++ Fizz Buzz File.py
@@ -1,20 +1,25 @@
 def play_fizz_buzz(number, numbers):
+    multi_1 = 3
+    multi_2 = 5
+    multi_3 = 7  
     try:
         for count in range (number, numbers):
-            if count >= 0:
-                if count % 3 == 0 and count % 5 == 0:
-                    print("Fizz Buzz")
-                elif count % 3 == 0:
-                    print("Fizz")
-                elif count % 5 == 0:
-                    print("Buzz")
-                else:
-                    print(count)        
+            returning_list = [] 
+            check = 0
+            if count > 0:
+                if count % multi_1 == 0:
+                    returning_list.append("Fizz")
+                    check += 1
+                if count % multi_2 == 0:
+                    returning_list.append("Buzz")
+                    check += 1
+                if count % multi_3 == 0:
+                    returning_list.append("Bizz")
+                    check += 1
+                elif check < 1:
+                    returning_list.append(str(count))
+            print(",".join(returning_list))      
     except TypeError:
         print("Error, this method only takes whole Integer numbers, please try again")
     except ZeroDivisionError:
         print("Fizz Buzz as a game needs something to work with, numbers passed cannot include Zero (0)") 
-    except NameError:
-        print("Error, this method only takes whole Integer numbers, please try again")
-
-play_fizz_buzz (1, 11)
