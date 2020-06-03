d7bbf13071ec12c56a62659b61b31e1dd8654870
Thomas <jackson996@btinternet.com>
2020-06-03 11:55:42 +0100

    we are fucked

--- Fizz Buzz File.py
+++ Fizz Buzz File.py
@@ -1,11 +1,11 @@
 def play_fizz_buzz(number, numbers):
     multi_1 = 3
     multi_2 = 5
-    multi_3 = 7  
+    multi_3 = 7
     try:
         for count in range (number, numbers):
             returning_list = [] 
-            check = 1
+            check = 0
             if count > 0:
                 if count % multi_1 == 0:
                     returning_list.append("Fizz")
@@ -23,4 +23,5 @@ def play_fizz_buzz(number, numbers):
         print("Error, this method only takes whole Integer numbers, please try again")
     except ZeroDivisionError:
         print("Fizz Buzz as a game needs something to work with, numbers passed cannot include Zero (0)") 
-    
+
+play_fizz_buzz(1, 22)    
